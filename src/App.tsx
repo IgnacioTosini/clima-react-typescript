bb9f4a0f07788f35cbb436181cf553869cc13705
ignacio tosini <ignaciotosini2002@gmail.com>
2024-12-24 15:28:52 -0300

    Final Proyecto Clima

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import styles from './App.module.css'
+import { Alert } from './components/Alert/Alert'
 import { Form } from './components/Form/Form'
+import { Spinner } from './components/Spinner/Spinner'
+import { WeatherDetail } from './components/WeatherDetail/WeatherDetail'
 import useWeather from './hooks/useWeather'
 
 function App() {
-  const { fetchWeather } = useWeather()
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather()
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
         <Form fetchWeather={fetchWeather}></Form>
-        <p>2</p>
+        {loading && <Spinner></Spinner>}
+        {hasWeatherData &&
+          <WeatherDetail weather={weather}></WeatherDetail>
+        }
+        {notFound &&
+          <Alert>
+            Ciudad no encontrada
+          </Alert>
+        }
       </div>
     </>
   )
